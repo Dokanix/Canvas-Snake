7f6016496dc237045ac7c29d694da0f9948fcb59
Dominik Piotrowicz <dom.piotrowicz@gmail.com>
2020-09-24 20:20:21 +0200

    Add Fruits

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector("canvas");
-const cellAdder = document.querySelector(".cell-adder");
+// const cellAdder = document.querySelector(".cell-adder");
 const slowButton = document.querySelector(".slower");
 const fastButton = document.querySelector(".faster");
 const header = document.querySelector("h1");
@@ -7,7 +7,14 @@ const c = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-let speed = 1;
+let speed = 3;
+
+class Fruit {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
 
 class Cell {
   constructor(x, y) {
@@ -48,6 +55,14 @@ class Player {
       gameOver();
     }
 
+
+    fruits.forEach((fruit, fruitIndex) => {
+      if (this.cells[this.cells.length - 1].x == fruit.x && this.cells[this.cells.length - 1].y == fruit.y) {
+        this.addCell();
+        fruits.splice(fruits[fruitIndex]);
+        spawnFruit();
+      }
+    })
   };
 
   addCell = () => {
@@ -79,22 +94,46 @@ const gameOver = () => {
   clearInterval(interval);
 }
 
-const player = new Player();
-cellAdder.addEventListener("click", player.addCell);
+const fruits = [];
+let round = 0;
 
-const draw = () => {
+const player = new Player();
+// cellAdder.addEventListener("click", player.addCell);
+
+const spawnFruit = () => {
+  let x = canvas.width / 2;
+  let y = canvas.height / 2;
+  let randomOffset = Math.floor(Math.random() * 10 - 5) * player.size; 
+  x = x + randomOffset;
+  randomOffset = Math.floor(Math.random() * 10 - 5) * player.size; 
+  y = y + randomOffset;
+
+  fruits.push(new Fruit(x, y));
+}
+
+spawnFruit();
+
+const update = () => {
   const directionText = document.querySelector(".direction");
   const pointsText = document.querySelector(".points");
   directionText.textContent = player.direction;
   pointsText.textContent = player.cells.length;
 
+  round++;
+
   c.clearRect(0, 0, canvas.width, canvas.height);
 
   player.movePosition();
 
+
   c.fillStyle = player.color;
   for (const cell of player.cells) {
     c.fillRect(cell.x, cell.y, player.size, player.size);
+  }
+
+  c.fillStyle = "purple";
+  for (const fruit of fruits) {
+    c.fillRect(fruit.x, fruit.y, player.size, player.size);
   }
 };
 
@@ -113,23 +152,23 @@ addEventListener("keydown", (event) => {
       player.direction = "down";
       break;
     case "n":
-      draw();
+      update();
       break;
     default:
       break;
   }
 });
 
-let interval = setInterval(draw, 1000 / speed);
+let interval = setInterval(update, 1000 / speed);
 
 slowButton.addEventListener("click", () => {
   speed /= 2;
   clearInterval(interval);
-  interval = setInterval(draw, 1000 / speed);
+  interval = setInterval(update, 1000 / speed);
 })
 
 fastButton.addEventListener("click", () => {
   speed *= 2;
   clearInterval(interval);
-  interval = setInterval(draw, 1000 / speed);
+  interval = setInterval(update, 1000 / speed);
 })
